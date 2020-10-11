ac5489fbcc23fc630107f9fa9d95757907f02a31
ElectroVen1k <65223599+ElectroVen1k@users.noreply.github.com>
2020-10-11 23:04:16 +0400

    Сделал блок с настройкой персонажа

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,15 @@
 'use strict';
 
 const setupField = document.querySelector('.setup');
+const buttonOpenSetup = document.querySelector('.setup-open');
+const buttonCloseSetup = setupField.querySelector('.setup-close');
+const nameInput = setupField.querySelector('.setup-user-name');
+const coatColorInput = setupField.querySelector('input[name=coat-color]');
+const eyesColorInput = setupField.querySelector('input[name=eyes-color]');
+const fireBallColorInput = setupField.querySelector('input[name=fireball-color]');
+const setupFireballElement = setupField.querySelector('.setup-fireball-wrap');
+const setupEyesElement = setupField.querySelector('.wizard-eyes');
+const setupCoatElement = setupField.querySelector('.wizard-coat');
 const wizardsListField = setupField.querySelector('.setup-similar');
 const wizardsListContainer = document.querySelector('.setup-similar-list');
 const wizardTemplate = document.querySelector('#similar-wizard-template')
@@ -47,7 +56,13 @@ const EYES_COLORS = [
   'green'
 ];
 
-setupField.classList.remove('hidden');
+const FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
 
 const getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -94,3 +109,89 @@ const randomWizards = getFakeWizardsList(WIZARDS_QUANTITY);
 wizardsListContainer.appendChild(randomWizards);
 
 wizardsListField.classList.remove('hidden');
+
+const onSetupFieldEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    closeSetupBlock();
+  }
+};
+
+const onSetupFieldButtonClosePress = function (evt) {
+  if (evt.key === 'Enter') {
+    closeSetupBlock();
+  }
+};
+
+const inFocusNameInputEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
+  }
+};
+
+const openSetupBlock = function () {
+  setupField.classList.remove('hidden');
+  buttonCloseSetup.addEventListener('keydown', onSetupFieldButtonClosePress);
+  buttonCloseSetup.addEventListener('click', closeSetupBlock);
+  document.addEventListener('keydown', onSetupFieldEscPress);
+  nameInput.addEventListener('keydown', inFocusNameInputEscPress);
+  setupCoatElement.addEventListener('click', swapCoatColor);
+  setupEyesElement.addEventListener('click', swapEyesColor);
+  setupFireballElement.addEventListener('click', swapFireballColor);
+};
+
+const closeSetupBlock = function () {
+  setupField.classList.add('hidden');
+  document.removeEventListener('keydown', onSetupFieldEscPress);
+  buttonCloseSetup.removeEventListener('keydown', onSetupFieldButtonClosePress);
+  buttonCloseSetup.removeEventListener('click', closeSetupBlock);
+  nameInput.removeEventListener('keydown', inFocusNameInputEscPress);
+  setupCoatElement.removeEventListener('click', swapCoatColor);
+  setupEyesElement.removeEventListener('click', swapEyesColor);
+  setupFireballElement.removeEventListener('click', swapFireballColor);
+};
+
+buttonOpenSetup.addEventListener('click', function () {
+  openSetupBlock();
+});
+
+buttonOpenSetup.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    openSetupBlock();
+  }
+});
+
+let idCoatColor = 1;
+
+const swapCoatColor = function () {
+  setupCoatElement.style.fill = COAT_COLORS[idCoatColor];
+  coatColorInput.value = COAT_COLORS[idCoatColor];
+  if (idCoatColor < COAT_COLORS.length - 1) {
+    idCoatColor = idCoatColor + 1;
+  } else {
+    idCoatColor = 0;
+  }
+};
+
+let idEyesColor = 1;
+
+const swapEyesColor = function () {
+  setupEyesElement.style.fill = EYES_COLORS[idEyesColor];
+  eyesColorInput.value = EYES_COLORS[idEyesColor];
+  if (idEyesColor < EYES_COLORS.length - 1) {
+    idEyesColor = idEyesColor + 1;
+  } else {
+    idEyesColor = 0;
+  }
+};
+
+let idFireballColor = 1;
+
+const swapFireballColor = function () {
+  setupFireballElement.style.backgroundColor = FIREBALL_COLORS[idFireballColor];
+  fireBallColorInput.value = FIREBALL_COLORS[idFireballColor];
+  if (idFireballColor < FIREBALL_COLORS.length - 1) {
+    idFireballColor = idFireballColor + 1;
+  } else {
+    idFireballColor = 0;
+  }
+};
